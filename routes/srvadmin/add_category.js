8da2d1c6d1c7b4b1bf7d330b7be51b02d06e94f6
Douglas Pollock <whatevermaan@hotmail.com>
2020-10-27 13:54:27 +0000

    added functionality for validate category name

--- routes/srvadmin/add_category.js
+++ routes/srvadmin/add_category.js
@@ -6,16 +6,29 @@ module.exports = function (router) {
   })
 
   router.post('/admin/add_category', (req, res) => {
+    const err = {
+      categoryName: {}
+    }
+
+    if (req.body.category_name === '') {
+      err.categoryName.blank = true
+      err.summarise = true
+    } else if (req.body.category_name.length > 45) {
+      err.portfolioName.tooLong = true
+      err.summarise = true
+    }
+
+    if (err.summarise) {
+      const reqjson = req.body
+      res.render('admin/add_category.html', { err, reqjson })
+    } else {
     queries.createCategory(req.body.category_name)
-      .then(
-        addedCategory => {
-          queries.searchCategories.getAll()
-            .then(
-              categories => {
-                res.render('admin/maintain_categories.html', { categories, addedCategory })
-              }
-            )
-        }
-      )
+      .then(addedCategory => {
+        queries.searchCategories.getAll()
+          .then(categories => {
+            res.render('admin/maintain_categories.html', { categories, addedCategory })
+          })
+      })
+    }
   })
 }
--- routes/srvadmin/edit_category.js
+++ routes/srvadmin/edit_category.js
@@ -3,25 +3,39 @@ const queries = require('../../app/db/queries')
 module.exports = function (router) {
   router.get('/admin/edit_category/:id', (req, res) => {
     queries.searchCategories.getById(req.params.id)
-      .then(
-        category => {
-          res.render('admin/edit_category.html', { category })
-        }
-      )
+      .then(category => {
+        res.render('admin/edit_category.html', { category })
+      })
   })
 
   router.post('/admin/edit_category/:id', (req, res) => {
-    queries.updateCategory(req.body.category_name, req.params.id)
-      .then(
-        category => {
+    const err = {
+      categoryName: {}
+    }
+
+    if (req.body.category_name === '') {
+      err.categoryName.blank = true
+      err.summarise = true
+    } else if (req.body.category_name.length > 45) {
+      err.portfolioName.tooLong = true
+      err.summarise = true
+    }
+
+    if (err.summarise) {
+      const reqjson = req.body
+      queries.searchCategories.getById(req.params.id)
+        .then(category => {
+          res.render('admin/edit_category.html', { category, err, reqjson })
+        })
+    } else {
+      queries.updateCategory(req.body.category_name, req.params.id)
+        .then(category => {
           const changeSuccess = true
           queries.searchCategories.getAll()
-            .then(
-              categories => {
-                res.render('admin/maintain_categories.html', { categories, changeSuccess })
-              }
-            )
-        }
-      )
+            .then(categories => {
+              res.render('admin/maintain_categories.html', { categories, changeSuccess })
+            })
+        })
+    }
   })
 }
